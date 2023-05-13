aaea91d3ac3f888da7a0f6a3dd057d8fb7591aec
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-05-13 17:46:41 +0300

    update task 005

--- 005_tmp/ManTest.java
+++ 005_tmp/ManTest.java
@@ -4,16 +4,13 @@ public class ManTest {
         Man man1 = new Man();
         man1.setName("Aleksey");
         System.out.println("Man1 = " + man1.getName());
-        
-        Man man2 = new Man();
-        man2.setName("Ivan");
-        System.out.println("Man2 = " + man2.getName());
 
-        Man man3 = man1;
-        System.out.println("Man3 = " + man3.getName());
-
-        man3.setName("Kulibin");
-        System.out.println("Man3 = " + man3.getName());
+        doSomething(man1);
         System.out.println("Man1 = " + man1.getName());
+
+    }
+
+    public static void doSomething(Man man) {
+        man.setName("Kulibin");
     }
 }
